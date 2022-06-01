fcdd1fa68021193f779c10fc5dad054e3352c8d2
Lisa Jennings <mrslmjennings@gmail.com>
2022-06-01 18:52:35 -0400

    Completed polybis portion of assignment

--- src/polybius.js
+++ src/polybius.js
@@ -8,7 +8,42 @@ const polybiusModule = (function () {
 
   function polybius(input, encode = true) {
     // your solution code here
+    
+    let result = "";
+    const encoder = {'a': '11', 'b': '21', 'c': '31', 'd': '41', 'e': '51', 'f': '12', 'g': '22', 'h': '32', 'i': '42','j': '42', 'k': '52', 'l': '13', 'm': '23', 'n': '33', 'o': '43', 'p': '53', 'q': '14', 'r': '24', 's': '34', 't': '44', 'u': '54', 'v': '15', 'w': '25', 'x': '35', 'y': '45', 'z': '55' };
+const decoder = {'11': 'a', '21': 'b', '31': 'c', '41': 'd', '51': 'e', '12': 'f', '22': 'g', '32': 'h','42':'i/j',  '52': 'k', '13': 'l', '23': 'm', '33': 'n', '43': 'o', '53': 'p', '14': 'q', '24': 'r', '34': 's', '44': 't', '54': 'u', '15': 'v', '25': 'w', '35': 'x', '45': 'y', '55': 'z' };
+if (encode == true){
+  for(let i=0;i<input.length;i++){
+    const char = input[i];
+    if(char.charCodeAt()!== 32){
+      result+=encoder[char];
+    }
+    else{
+      result+=char;
+    }
+        
   }
+}  
+if (encode == false){
+  let trimmed = input.replace(/\s/g, '');
+  if(trimmed.length%2==1) return false;
+  let increment =1;
+  for(let i=0;i<input.length;i+=increment){
+    if(input[i].charCodeAt()!==32 && input[i+1].charCodeAt()!==32){
+      const char = input[i] + input[i+1];
+      result+=decoder[char];
+      increment=2;
+    }
+    else{
+      char = input[i];
+      result+=char;
+      increment=1;
+    }
+  }
+}  
+console.log(result);
+return result;
+}
 
   return {
     polybius,
