e8c56947e32a064e509e87d3088fc033467225b5
Lisa Jennings <mrslmjennings@gmail.com>
2022-06-01 20:12:47 -0400

    Completed substitution assignment

--- src/substitution.js
+++ src/substitution.js
@@ -8,6 +8,42 @@ const substitutionModule = (function () {
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+    if(!alphabet) return false;
+    if(alphabet.length!==26 || new Set(alphabet).size !== alphabet.length) return false;
+    let result = "";
+    const originalAlphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const codedAlphabet = {};
+    const decodedAlphabet = {};
+if (encode == true){
+  for(let i=0;i<originalAlphabet.length;i++){
+    codedAlphabet[originalAlphabet[i]]=alphabet[i];
+  }
+  for(let i=0;i<input.length;i++){
+    const char = input[i];
+    if(char.charCodeAt()!==32){ 
+      result+=codedAlphabet[char];
+      }
+      else{
+        result = result + char;
+      }
+  }
+}  
+if (encode == false){
+  for(let i=0;i<originalAlphabet.length;i++){
+    decodedAlphabet[alphabet[i]]=originalAlphabet[i];
+  }
+  for(let i=0;i<input.length;i++){
+    const char = input[i];
+    if(char.charCodeAt()!==32){ 
+    result+=decodedAlphabet[char];
+    }
+    else{
+      result = result + char;
+    }
+  }
+}  
+console.log(result);
+return result;
   }
 
   return {
