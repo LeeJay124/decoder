34876497d58c1fafc508cde51beeba3ea1d5ebbf
Lisa Jennings <mrslmjennings@gmail.com>
2022-06-02 09:04:22 -0400

    Caesar equation fix

--- src/caesar.js
+++ src/caesar.js
@@ -12,44 +12,69 @@ const caesarModule = (function () {
     let encoded = "";
     input = input.toLowerCase();
     if(encode == false){ 
-      for(let i=0;i<input.length;i++){
-        const charCode = input[i];
-        if(charCode.charCodeAt() < 97 || charCode.charCodeAt() >
-        122) {
-          encoded += charCode;
-      }
-      else{
-        let newCharCode = charCode.charCodeAt(charCode + (+shift));
-        newCharCode = newCharCode - shift;
-        if(newCharCode < 97 || newCharCode >122){
-          newCharCode = newCharCode - 122 + 96;
-        }
-        if(newCharCode == 120 || newCharCode == 121 || newCharCode == 122){
-          newCharCode = newCharCode - 122 + 122;
-        }
-        encoded += String.fromCharCode(newCharCode);
-      }
-    }
+      shift = -shift;
+    //   for(let i=0;i<input.length;i++){
+    //     const charCode = input[i];
+    //     if(charCode.charCodeAt() < 97 || charCode.charCodeAt() >
+    //     122) {
+    //       encoded += charCode;
+    //   }
+    //   else{
+    //     let newCharCode = charCode.charCodeAt(charCode + (+shift));
+    //     newCharCode = newCharCode - shift;
+    //     if(newCharCode < 97 || newCharCode >122){
+    //       newCharCode = newCharCode - 122 + 96;
+    //     }
+    //     if(newCharCode == 120 || newCharCode == 121 || newCharCode == 122){
+    //       newCharCode = newCharCode - 122 + 122;
+    //     }
+    //     encoded += String.fromCharCode(newCharCode);
+    //   }
+    // }
   }
-  if(encode == true){ 
-    for(let i=0;i<input.length;i++){
-      const charCode = input[i];
-      if(charCode.charCodeAt() < 97 || charCode.charCodeAt() >
-        122) {
-        encoded += charCode;
-    }
-    else{
-      let newCharCode = charCode.charCodeAt(charCode - (+shift));
-      newCharCode = newCharCode + (shift%26);
-      if(newCharCode < 97 || newCharCode >122){
-        newCharCode = newCharCode - 122 + 96;
+  //if(encode == true){ 
+  //   for(let i=0;i<input.length;i++){
+  //     const charCode = input[i];
+  //     if(charCode.charCodeAt() < 97 || charCode.charCodeAt() >
+  //       122) {
+  //       encoded += charCode;
+  //   }
+  //   else{
+  //     let newCharCode = charCode.charCodeAt(charCode - (+shift));
+  //     newCharCode = newCharCode + (shift%26);
+  //     if(newCharCode < 97 || newCharCode >122){
+  //       newCharCode = newCharCode - 122 + 96;
+  //     }
+  //     if(newCharCode == 120 || newCharCode == 121 || newCharCode == 122){
+  //       newCharCode = newCharCode - 122 + 122;
+  //     }
+  //     encoded += String.fromCharCode(newCharCode);
+  //   }
+  // }
+//}
+for (let i = 0; i < input.length; i++) {
+  const charCode = input.charCodeAt(i);
+
+  if (
+      (charCode < 65 || charCode > 122) ||
+      (charCode > 90 && charCode < 97)
+  ) {
+      encoded += input[i];
+  } else {
+      let newCharCode = charCode + Math.ceil(shift % 26);
+
+      if (charCode >= 97 && newCharCode >= 122) {
+          newCharCode = newCharCode - 122 + 96;
       }
-      if(newCharCode == 120 || newCharCode == 121 || newCharCode == 122){
-        newCharCode = newCharCode - 122 + 122;
+      if (charCode <= 90 && newCharCode >= 90) {
+          newCharCode = newCharCode - 90 + 64;
+      }
+      if(newCharCode<97){
+        newCharCode = newCharCode + 26;
       }
       encoded += String.fromCharCode(newCharCode);
-    }
   }
+
 }
     console.log(encoded);
     return encoded;
